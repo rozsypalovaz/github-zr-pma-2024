d76e5bcacd6a440c8ea351954cfe9de2e8fe11a0
rozsypalovaz <zuzana.rozsypalova@tul.cz>
2025-01-14 18:31:18 +0100

    Projekt č. 5 (nefunkční)

---
+++ MyAppViceAktivit/app/src/main/java/com/example/myappviceaktivit/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapp006moreactivities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myappviceaktivit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        val btnSecond = findViewById<Button>(R.id.btnSecond)
+        val etNickname = findViewById<EditText>(R.id.etNickname)
+
+        btnSecond.setOnClickListener {
+            val nickname = etNickname.text.toString() // získáme text z edit text pole
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("NICK_NAME", nickname)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
---
+++ MyAppViceAktivit/app/src/main/java/com/example/myappviceaktivit/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapp006moreactivities
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myappviceaktivit.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val twInfo = findViewById<TextView>(R.id.twInfo)
+
+        // Načtení dat z intentu
+        val nickname = intent.getStringExtra("NICK_NAME")
+        twInfo.text = "Data z první aktivity. Jméno: $nickname"
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+    }
+}
