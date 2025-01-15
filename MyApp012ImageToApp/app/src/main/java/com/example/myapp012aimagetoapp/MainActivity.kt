ecc72ee115dda397c31557fde566ce1aba7a8c38
rozsypalovaz <zuzana.rozsypalova@tul.cz>
2025-01-15 17:24:03 +0100

    Aktualizace, přídání toastbaru

--- MyApp012ImageToApp/app/src/main/java/com/example/myapp012aimagetoapp/MainActivity.kt
+++ MyApp012ImageToApp/app/src/main/java/com/example/myapp012aimagetoapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myapp012aimagetoapp
 
+import android.graphics.Bitmap
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.myapp012aimagetoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,13 +22,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
-            uri: Uri? -> binding.ivImage.setImageURI(uri)
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            binding.ivImage.setImageURI(uri)
         }
 
         binding.btnTakeImage.setOnClickListener {
             getContent.launch("image/*")
         }
 
+        binding.btnBlackWhite.setOnClickListener {
+            if (binding.ivImage.drawable == null) {
+                Toast.makeText(this, "Nebyl nahrán žádný obrázek!", Toast.LENGTH_SHORT).show()
+            } else {
+                convertToBlackWhite()
+            }
+        }
+    }
+
+    private fun convertToBlackWhite() {
+        val drawable = binding.ivImage.drawable as BitmapDrawable
+        val bitmap = drawable.bitmap
+
+        val colorMatrix = ColorMatrix()
+        colorMatrix.setSaturation(0f)
+        val filter = ColorMatrixColorFilter(colorMatrix)
+
+        val blackWhiteDrawable = BitmapDrawable(resources, Bitmap.createBitmap(bitmap))
+        blackWhiteDrawable.colorFilter = filter
+
+        binding.ivImage.setImageDrawable(blackWhiteDrawable)
     }
 }
